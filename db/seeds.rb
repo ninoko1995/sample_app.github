732c82e07d88c54808df32b14b6de92eb5ce6be0
ninoko1995 <ninomiya.infratpo@gmail.com>
2016-06-22 11:17:45 +0000

    reset seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,9 @@
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
-             password_confirmation: "foobar"
-             admin: true)
+             password_confirmation: "foobar",
+             admin:true
+             )
 
 99.times do |n|
   name  = Faker::Name.name
